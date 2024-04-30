5acc7c39d9a9743671deec5cd1b88151c4dbf8fe
Sebastian Mosquera <sebimosquera@gmail.com>
2024-04-30 19:33:49 -0300

    fix baseURL

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const productApi = axios.create({
-    baseURL: import.meta.env.VITE_API_DEV
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 productApi.interceptors.request.use(config => {
